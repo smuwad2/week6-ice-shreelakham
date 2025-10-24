e6531808f506175655e75d18836ef5253248394f
shreelakham <shreelakham.2024@smu.edu.sg>
2025-10-24 06:47:32 +0000

    done ex4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -2,24 +2,55 @@
     export default { 
         data() {
             return {
-                // Add code here
-            }
-        },
-        methods: { 
+                // Add code 
+                redbtn : "btn btn-danger",
+                bluebtn : "btn btn-primary",
+                counter : 0,
+                id : "demo1",
+                currentClass : "btn btn-primary",
+                Blueactive : true,
+                Redactive : false
+
+        }
+    },
+        methods : { 
    			// Add code here
+            addClass(){
+            this.counter +=1;
+            if(this.counter%2 ==1){
+                this.currentClass = this.redbtn;
+                this.Redactive = true;
+                this.Blueactive = false
+            }
+            else{
+                this.currentClass = this.bluebtn;
+                this.Redactive = false;
+                this.Blueactive = true
+            }
+            
+
+
+
+            }
+            
+
 		}
-    }
+    
+}
 </script>
 
 <template>
     
     <!-- Modify code here -->
     <div class="container">
-        <div v-bind:id="id" class="m-2">
+        <div v-bind:id="id" class="m-2" v-bind:class="{'blueBox' : Blueactive, 'redBox': Redactive}">
             div ID : {{id}} 
         </div>
         
-        <button type="button">Change Color</button>
+        <button v-bind:class="[currentClass]"
+        v-on:click="addClass"
+        
+        type="button">Change Color</button>
     </div>
 
 </template>
