9fa01cf1f0ad29964d84ef5c55a49edb67b2b1e4
shreelakham <shreelakham.2024@smu.edu.sg>
2025-10-25 12:52:26 +0000

    done qn 5

--- src/components/Ex5.vue
+++ src/components/Ex5.vue
@@ -15,6 +15,36 @@ export default {
     
     methods: {
         // Add code here
+        getTheme(){
+            
+            this.currentThemeIndex += 1; 
+
+            // 2. Wrap around (must check AFTER incrementing)
+            if(this.currentThemeIndex === 3){
+                this.currentThemeIndex = 0;
+            }
+            
+            // 3. APPLY THE NEW THEME
+            const currentTheme = this.themes[this.currentThemeIndex];
+
+            // Now, after the first click, currentThemeIndex is 1, so it applies 'light'
+            if(currentTheme === 'dark'){
+                this.bgColor = "#333" ;
+                this.textColor ="#fff";
+            }
+            else if(currentTheme === 'light')
+            {
+                this.bgColor = "#fff" ;
+                this.textColor ="#000";
+            }
+            else if(currentTheme === 'neon')
+            {
+                this.bgColor = "#39ff14" ;
+                this.textColor ="#000";
+            }
+
+            
+        }
 
     }
 }
@@ -39,7 +69,7 @@ export default {
             <input id="imageUrl" v-model="imageUrl" placeholder="https://example.com/me.jpg"><br><br>
 
             <label>Theme Presets:</label><br>
-            <button class="theme-button">Cycle theme</button> <!-- click button to cycle through the themes -->
+            <button class="theme-button" v-on:click="getTheme">Cycle theme</button> <!-- click button to cycle through the themes -->
             <!-- Dark theme: background-color: #333, text-color: #fff -->
             <!-- Light theme: background-color: #fff, text-color: #000  -->
             <!-- Neon theme: background-color: #39ff14, text-color: #000 -->
@@ -48,7 +78,10 @@ export default {
         <!-- Preview Section -->
         <div class="preview-section">
             <h2>Live Preview</h2>
-            <div class="preview-card"> <!-- Add code here to set background color and text color -->
+            <div class="preview-card" v-bind:style="{ 'background-color': bgColor, 'color': textColor}"> 
+                <!-- Add code here to set background color and text color -->
+                 
+
                 <img :src="imageUrl" class="preview-img">
                 <h3>{{ name || 'Your Name' }}</h3>
                 <h4>{{ job || 'Job Title' }}</h4>
